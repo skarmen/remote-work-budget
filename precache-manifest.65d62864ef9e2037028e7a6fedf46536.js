87a7f29c0d5affca8467ca264aaed790d327175f
skarmen <karmennsalim@gmail.com>
2020-10-13 15:16:38 -0400

    Updates

---
+++ precache-manifest.65d62864ef9e2037028e7a6fedf46536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1ead060ec7c357e23a1568dd08b80b",
+    "url": "/remote-work-budget/index.html"
+  },
+  {
+    "revision": "fcd2a3afc40973f49d7e",
+    "url": "/remote-work-budget/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "4d1649962b7769505002",
+    "url": "/remote-work-budget/static/css/main.de300530.chunk.css"
+  },
+  {
+    "revision": "fcd2a3afc40973f49d7e",
+    "url": "/remote-work-budget/static/js/2.fd434a3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/remote-work-budget/static/js/2.fd434a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1649962b7769505002",
+    "url": "/remote-work-budget/static/js/main.282e4b93.chunk.js"
+  },
+  {
+    "revision": "c408ba7a223d264f688c",
+    "url": "/remote-work-budget/static/js/runtime-main.ee8cae69.js"
+  }
+]);
